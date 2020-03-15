81528e5a8ca63232a98b91ed2a2b7151e94b3de7
jchery25 <jchery25@yahoo.com>
2020-03-14 23:53:29 -0400

    Added User test

--- Development/src/test/java/com/neu/prattle/modelTest/UserModelTest.java
+++ Development/src/test/java/com/neu/prattle/modelTest/UserModelTest.java
@@ -2,9 +2,24 @@ package com.neu.prattle.modelTest;
 
 import com.neu.prattle.model.User;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserModelTest {
+
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new User();
+        user.setFirstName("User");
+        user.setLastName("One");
+        user.setPassword("TestOne");
+        user.setUsername("user1");
+        user.setTimezone("GMT");
+    }
 
   @Test
   public void testUserModel() {
@@ -12,7 +27,27 @@ public class UserModelTest {
   }
 
     @Test
-    public void testUser(){
+    public void testUserFirstName(){
+        assertEquals("User",user.getFirstName());
+    }
 
+    @Test
+    public void testUserLastName(){
+        assertEquals("One",user.getLastName());
+    }
+
+    @Test
+    public void testUserUsername(){
+        assertEquals("user1",user.getUsername());
+    }
+
+    @Test
+    public void testUserTimezone(){
+        assertEquals("GMT",user.getTimezone());
+    }
+
+    @Test
+    public void testUserPassword(){
+        assertEquals("TestOne",user.getPassword());
     }
 }

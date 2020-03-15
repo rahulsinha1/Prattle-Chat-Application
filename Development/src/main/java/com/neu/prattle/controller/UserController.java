9651dee47f0a7c59b3c222b6fe9cb0bc917e73ce
jchery25 <jchery25@yahoo.com>
2020-03-15 00:47:00 -0400

    Removed code smeels

--- Development/src/main/java/com/neu/prattle/controller/UserController.java
+++ Development/src/main/java/com/neu/prattle/controller/UserController.java
@@ -6,9 +6,7 @@ import com.neu.prattle.service.UserService;
 import com.neu.prattle.service.UserServiceImpl;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
 import javax.ws.rs.POST;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;

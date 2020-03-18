332bd3e5e55439db6ffed1384e6826eade80c009
jchery25 <jchery25@yahoo.com>
2020-03-18 17:36:53 -0400

    Working on testing

--- Development/src/test/java/com/neu/prattle/controllerTest/UserControllerTest.java
+++ Development/src/test/java/com/neu/prattle/controllerTest/UserControllerTest.java
@@ -3,16 +3,22 @@ package com.neu.prattle.controllerTest;
 import com.neu.prattle.controller.UserController;
 import com.neu.prattle.model.User;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserControllerTest {
 
-  UserController userController = new UserController();
+  UserController userController ;
+  User user;
+
+  @Before
+  public void setUp(){
+      user = new User("Test","Test", "testtest", "password", "GMT");
+  }
 
   @Test
   public void testCreateUserAccount(){
-    User u = new User("TestUser234");
-    userController.createUserAccount(u);
-    userController.createUserAccount(u);
+
   }
+
 }

ae9aaae25d0328eda3c558ffb898deb2556baa3f
jchery25 <jchery25@yahoo.com>
2020-03-20 19:13:01 -0400

    Added test for get user and create user

--- Development/src/test/java/com/neu/prattle/controllerTest/UserControllerTest.java
+++ Development/src/test/java/com/neu/prattle/controllerTest/UserControllerTest.java
@@ -3,27 +3,61 @@ package com.neu.prattle.controllerTest;
 import com.neu.prattle.controller.UserController;
 import com.neu.prattle.model.User;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
+import com.neu.prattle.service.UserService;
+import com.neu.prattle.service.UserServiceImpl;
+import net.sf.json.JSON;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 
+import javax.ws.rs.core.Response;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
-  private UserController userController;
-  User user;
+    UserController userController;
+    private UserService userService;
+    private User user;
+    private User user1;
 
-  @Before
+
+    @Before
   public void setUp(){
-      user = new User("Test","Test", "testtest", "password", "GMT");
+      userService = UserServiceImpl.getInstance();
+      userController = new UserController();
+      user = new User("User", "Test", "testtest","pass1234","GMT");
+      user1 = new User("User", "Test", "anotherTest","pass1234","GMT");
   }
+
 
   @Test
-  public void testCreateUserAccount() throws Exception {
-
+  public void testCreateUser() {
+      Response response = userController.createUserAccount(user);
+      assertEquals(200, response.getStatus());
   }
 
+    @Test
+    public void testCreateUserSameUser() {
+        userController.createUserAccount(user);
+        Response response = userController.createUserAccount(user);
+        assertEquals(409, response.getStatus());
+    }
+
+  @Test
+  public void testGetUser(){
+      userController.createUserAccount(user1);
+     Response response = userController.getUser("anotherTest");
+     assertTrue(response.hasEntity());
+  }
+
+    @Test
+    public void testGetUserThatDoesNotExist(){
+        Response response = userController.getUser("DoNotExit");
+        assertEquals(409, response.getStatus());
+    }
 }

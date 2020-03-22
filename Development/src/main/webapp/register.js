30ff393fc24237ea0caafbf126e2ec8b35ab3b81
jchery25 <jchery25@yahoo.com>
2020-03-22 16:10:49 -0400

    Updated register

--- Development/src/main/webapp/register.js
+++ Development/src/main/webapp/register.js
@@ -7,9 +7,9 @@
  * @param password is the password of the user.
  * @returns {boolean} true if fields are not empty.
  */
-function fieldsNotEmpty(first_name, last_name, username, confirm_password, password) {
-    return first_name.trim() !== "" || last_name.trim()  !== "" || username.trim()  !== ""||
-        confirm_password.trim()  !== "" || password.trim()  !== "";
+function fieldsNotEmpty(first_name, last_name, username, confirm_password, password, timezone) {
+    return first_name.trim() !== "" && last_name.trim()  !== "" && username.trim()  !== "" &&
+        confirm_password.trim()  !== "" && password.trim()  !== "" && timezone.trim() !== "";
 }
 
 /**
@@ -28,7 +28,7 @@ function register(){
 
     displayMessage.innerText = "";
 
-    if(fieldsNotEmpty(first_name, last_name, username, confirm_password, password)){
+    if(fieldsNotEmpty(first_name, last_name, username, confirm_password, password, timezone)){
         if(password === confirm_password){
 
             const user_data = {
@@ -39,21 +39,21 @@ function register(){
                 "password": password
             };
 
-            fetch('http://localhost:8080/prattle/rest/user/create', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user_data),
-            })
-                .then((data) => {
-                    displayMessage.innerText = "Successfully Registered";
-                    console.log('Success:', data);
-                })
-                .catch((error) => {
+                fetch('http://localhost:8080/prattle/rest/user/create', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(user_data),
+                }).then((response)=>{
+                    if(!response.ok){
+                        displayMessage.innerText = "Unsuccessfully Registered";
+                    } else {
+                        displayMessage.innerText = "Successfully Registered";
+                    }
+                }).catch((error)=>{
                     displayMessage.innerText = "Unsuccessfully Registered";
-                    console.log('Error:', error);
-                });
+                })
         } else {
             displayMessage.innerText = "Password and Confirm Password are not the same.";
         }

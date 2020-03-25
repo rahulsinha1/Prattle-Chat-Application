903400bd3258f0bf236316d9825d8fc13625c567
jchery25 <jchery25@yahoo.com>
2020-03-24 22:34:43 -0400

    Added test for Group

--- Development/src/test/java/com/neu/prattle/modelTest/GroupModelTest.java
+++ Development/src/test/java/com/neu/prattle/modelTest/GroupModelTest.java
@@ -11,22 +11,32 @@ import com.neu.prattle.service.UserServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+
+
 
 public class GroupModelTest {
 
   private GroupService groupService;
   private UserService userService;
+  private User user;
+
+  private Group group;
 
   @Before
   public void setUp() {
     groupService = GroupServiceImpl.getInstance();
     userService = UserServiceImpl.getInstance();
+
+    user = new User("test","test","Test","Test","Test");
+    group = new Group("Test","This is a test", "Test","password",true);
   }
 
   @Test
@@ -40,16 +50,75 @@ public class GroupModelTest {
     User u = new User("testuser800");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     g.setDescription("test");
-    g.setPrivate(false);
+    g.setIsGroupPrivate(false);
     Long currentTimeStamp = new Date().getTime();
     g.setCreatedOn(currentTimeStamp.toString());
     groupService.createGroup(g);
     assertEquals("test",g.getDescription());
-    assertFalse(g.isPrivate());
+    assertFalse(g.getIsGroupPrivate());
     assertFalse(g.equals(new Group("GROUP2")));
   }
 
+  @Test
+    public void testGetName(){
+    assertEquals("Test",group.getName());
+  }
+
+    @Test
+    public void testGetDescription(){
+        assertEquals("This is a test",group.getDescription());
+    }
+
+    @Test
+    public void testIsPrivate(){
+        assertTrue(group.getIsGroupPrivate());
+    }
+
+    @Test
+    public void testGetPassword(){
+        assertEquals("password",group.getPassword());
+    }
+
+    @Test
+    public void testGetCreatedOn(){
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//dd/MM/yyyy
+        Date now = new Date();
+        String strDate = sdfDate.format(now);
+        assertEquals(strDate,group.getCreatedOn());
+    }
+
+    @Test
+    public void testGetId(){
+        assertEquals(0,group.getId());
+    }
+
+    @Test
+    public void testModerators(){
+        List<Moderator> moderators = new ArrayList<>();
+        Moderator m = new Moderator("Moderator800");
+        moderators.add(m);
+        group.setModerators(moderators);
+
+        assertEquals("Moderator800",group.getModerators().get(0).getUsername());
+    }
+
+    @Test
+    public void getCreatedBy(){
+      assertEquals("Test", group.getCreatedBy());
+    }
+
+    @Test
+    public void setCreatedBy(){
+      group.setCreatedBy("Jon");
+        assertEquals("Jon", group.getCreatedBy());
+    }
+
+    @Test
+    public void setGroupId(){
+        group.setId(100);
+        assertEquals(100,group.getId());
+    }
 }
--- Development/src/test/java/com/neu/prattle/serviceTest/GroupServiceImplTest.java
+++ Development/src/test/java/com/neu/prattle/serviceTest/GroupServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.neu.prattle.serviceTest;
 
+import com.neu.prattle.exceptions.GroupAlreadyPresentException;
+import com.neu.prattle.exceptions.UserAlreadyPresentException;
 import com.neu.prattle.model.Group;
 import com.neu.prattle.model.Moderator;
 import com.neu.prattle.model.User;
@@ -14,6 +16,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
+
 public class GroupServiceImplTest {
 
   private GroupService groupService;
@@ -21,19 +25,30 @@ public class GroupServiceImplTest {
   private static final String TESTUSER = "testuser";
   private static final String TESTMODERATOR = "testmoderator";
   private Group group = new Group();
+  private Group group2;
   private static final String TESTGROUPNAME = "testgroupname";
 
 
   @Before
   public void setUp() {
+      group2 = new Group("Test","This is a Test.","TestTest","Test", false);
     groupService = GroupServiceImpl.getInstance();
     userService = UserServiceImpl.getInstance();
+
+
   }
 
   @Test
   public void testCreateGroup() {
     setMocksForGroupCreation();
     groupService.createGroup(group);
+  }
+
+  @Test(expected = GroupAlreadyPresentException.class)
+  public void testGroupCreation(){
+      groupService.createGroup(group2);
+      groupService.createGroup(group2);
+      assertFalse(false);
   }
 
   @Test
@@ -75,7 +90,7 @@ public class GroupServiceImplTest {
     User u = new User("testuser4");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     groupService.createGroup(g);
     groupService.updateGroup(g);
@@ -90,7 +105,7 @@ public class GroupServiceImplTest {
     users.add(u);
     List<Moderator> moderators = new ArrayList<>();
     moderators.add(m);
-    group.setUsers(users);
+    group.setMembers(users);
     group.setName(TESTGROUPNAME);
     group.setModerators(moderators);
   }
@@ -107,7 +122,7 @@ public class GroupServiceImplTest {
     User u = new User("testuser5");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     groupService.createGroup(g);
     groupService.deleteGroup(g);
@@ -125,7 +140,7 @@ public class GroupServiceImplTest {
     User u = new User("testuser6");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     groupService.createGroup(g);
     groupService.getAllGroups();
@@ -143,7 +158,7 @@ public class GroupServiceImplTest {
     User u = new User("testuser7");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     groupService.createGroup(g);
     groupService.getAllGroupsByUsername("testuser7");
@@ -166,7 +181,7 @@ public class GroupServiceImplTest {
     User u = new User("testuser10000000");
     List<User> users = new ArrayList<>();
     users.add(u);
-    g.setUsers(users);
+    g.setMembers(users);
     userService.addUser(u);
     groupService.createGroup(g);
     groupService.getGroupByName("TESTGROUP10");

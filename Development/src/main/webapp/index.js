485600401c30de2db141fbb97802644181618892
jainhar <jain.har@husky.neu.edu>
2020-03-28 07:50:25 -0400

    fixing view groups

--- Development/src/main/webapp/index.js
+++ Development/src/main/webapp/index.js
@@ -1,114 +1,30 @@
-let groupForm = document.getElementById("create_group_form");
-let viewGroup = document.getElementById("view_group_section");
-
+let createForm = document.getElementById("create_group_form");
+let viewAllGroupUserIsApartOf = document.getElementById("view_group_section");
+let deleteGroupForm = document.getElementById("delete_group_form");
+let moderatorOfGroup = document.getElementById("moderatorOfGroup");
+let selectedGroup = moderatorOfGroup.value;
 closeAllDisplay();
-
 function closeAllDisplay(){
-    groupForm.style.display = "none";
-    viewGroup.style.display = "none";
+    createForm.style.display = "none";
+    viewAllGroupUserIsApartOf.style.display = "none";
+    deleteGroupForm.style.display = "none";
 }
-
 function createGroup(){
     closeAllDisplay();
-    groupForm.style.display = "block";
+    createForm.style.display = "block";
 }
-
-function viewGroupMethod(){
-    closeAllDisplay()
-    viewGroup.style.display = "block";
-
-    fetch('http://localhost:8080/prattle/rest/group/getUser/'+ localStorage.getItem('username'))
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error)=> {
-            displayMessage.innerText = error;
-        })
-}
-
-function submitGroupCreation(){
-    let groupName = document.groupForm.groupName.value;
-    let description = document.groupForm.description.value;
-    let displayMessage = document.getElementById("create_group_message");
-
-    displayMessage.innerText = "";
-
-    if( groupName.trim() !== ""){
-        const group_data = {
-            "name": groupName,
-            "description": description,
-            "createdBy": localStorage.getItem('username'),
-            "isGroupPrivate": false,
-            "password": "",
-        };
-
-        fetch('http://localhost:8080/prattle/rest/group/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(group_data),
-        }).then((response)=>{
-            if(!response.ok){
-                displayMessage.innerText = "Group already exist. Try another group name.";
-            } else {
-                displayMessage.innerText = "Successfully Created";
-            }
-        }).catch((e)=>{
-            displayMessage.innerText = "Unsuccessfully Created";
-        })
-
-        fetch('http://localhost:8080/prattle/rest/group/addModerator/' + groupName + '/' + localStorage.getItem('username'),{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(group_data),
-        }).then((response)=>{
-            if(!response.ok){
-            displayMessage.innerText = "Group already exist. Try another group name.";
-        } else {
-            displayMessage.innerText = "Successfully Created group with moderator";
-        }
-    }).catch((e)=>{
-            displayMessage.innerText = "Unsuccessfully Created";
-    })
-
-        fetch('http://localhost:8080/prattle/rest/group/addUser/' + groupName + '/' + localStorage.getItem('username'),{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(group_data),
-        }).then((response)=>{
-            if(!response.ok){
-            displayMessage.innerText = "Group already exist. Try another group name.";
-        } else {
-            displayMessage.innerText = "Successfully Created group with moderator";
-        }
-    }).catch((e)=>{
-            displayMessage.innerText = "Unsuccessfully Created";
-    })
-    }
-}
-function clear(){
-
-}
-
 function viewGroupButton(){
     closeAllDisplay()
     viewAllGroupUserIsApartOf.style.display = "block";
     let displayMessage = document.getElementById("view_group_message");
     let groupsParticipation = document.getElementById("groupsParticipation");
-    fetch('http://localhost:8080/prattle/rest/group//getAllUserGroups//'+ localStorage.getItem('username'))
+    fetch('http://localhost:8080/prattle/rest/group/getAllUserGroups/'+ localStorage.getItem('username'))
         .then((response) => {
         return response.json();
 })
 .then((groupData) => {
         console.log(groupData);
+    groupsParticipation.innerHTML = "";
     for(group in groupData){
         groupsParticipation.innerHTML += "<option value=" + groupData[group].name + ">" + groupData[group].name + "</option>";
     }
@@ -117,8 +33,6 @@ function viewGroupButton(){
         displayMessage.innerText = error;
 })
 }
-
-
 function deleteGroup() {
     closeAllDisplay();
     deleteGroupForm.style.display = "block";
@@ -162,7 +76,38 @@ function deleteGroupButton(){
         displayMessage.innerText = error.toString();
 })
 }
-
 function submitGroupView(){
     console.log(groupsParticipation.value)
+}
+function submitGroupCreation(){
+    let groupName = document.createGroupForm.groupName.value;
+    let description = document.createGroupForm.description.value;
+    let displayMessage = document.getElementById("create_group_message");
+    displayMessage.innerText = "";
+    if( groupName.trim() !== ""){
+        const group_data = {
+            "name": groupName,
+            "description": description,
+            "createdBy": localStorage.getItem('username'),
+            "isGroupPrivate": false,
+            "password": "",
+        };
+        fetch('http://localhost:8080/prattle/rest/group/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(group_data),
+        }).then((response)=>{
+            if(!response.ok){
+            displayMessage.innerText = "Group already exist. Try another group name.";
+        } else {
+            displayMessage.innerText = "Successfully Created";
+        }
+    }).catch((e)=>{
+            displayMessage.innerText = "Unsuccessfully Created";
+    })
+    }
+}
+function clear(){
 }

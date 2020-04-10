f1c1bf42d161f14d0bb62e5ff884107596defc0f
jainhar <jain.har@husky.neu.edu>
2020-04-10 17:22:36 -0400

    fixing group gov tracking

--- Development/src/main/java/com/neu/prattle/service/UserServiceImpl.java
+++ Development/src/main/java/com/neu/prattle/service/UserServiceImpl.java
@@ -88,7 +88,13 @@ public class UserServiceImpl implements UserService {
   @Override
   public List<Group> findGroupsByName(String name) {
     if (isRecordExist(name)) {
-      User user = findUserByUsername(name);
+      User user;
+      try {
+        user = findUserByUsername(name);
+      }
+      catch (UserDoesNotExistException u) {
+        throw new UserDoesNotExistException(u.getMessage());
+      }
       return user.getGroupParticipant();
     }
     return Collections.emptyList();
